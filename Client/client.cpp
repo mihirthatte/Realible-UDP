49011b768e7fef3e0ae7ed5359b2686b44289a77
Manan Vinod Papdiwala <manpapdi@silo.soic.indiana.edu>
2018-03-17 00:40:27 -0400

    Client receives the data from server with header.

--- Client/client.cpp
+++ Client/client.cpp
@@ -2,14 +2,118 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <cstdlib>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
 
-#define BUFFSIZE 1456
+#define BUFFSIZE 1468
+#define RECVWINDOW 65535
+#define TIMEOUT_MS 100000
+#define MAXFILESIZE 10000000
 
 using namespace std;
+
+void generateRequest(string file_name, char* buffer, unsigned int sequenceNumber, unsigned int acknowledgementNumber, unsigned int receiveWindow){
+  unsigned char bytes[4];
+  bytes[0] = (sequenceNumber >> 24) & 0XFF;
+  bytes[1] = (sequenceNumber >> 16) & 0XFF;
+  bytes[2] = (sequenceNumber >> 8) & 0XFF;
+  bytes[3] = sequenceNumber & 0XFF;
+
+
+
+  memcpy(buffer, bytes, 4);
+
+
+  bytes[0] = (acknowledgementNumber >> 24) & 0XFF;
+  bytes[1] = (acknowledgementNumber >> 16) & 0XFF;
+  bytes[2] = (acknowledgementNumber >> 8) & 0XFF;
+  bytes[3] = acknowledgementNumber & 0XFF;
+
+  memcpy(buffer+4, bytes, 4);
+  bytes[0] = (receiveWindow >> 8) & 0XFF;
+  bytes[1] = receiveWindow & 0XFF;
+  bytes[2] = 0;
+  bytes[3] = 0;
+
+  memcpy(buffer+8, bytes, 4);
+  memcpy(buffer+12, file_name.c_str(), file_name.size()+1);
+
+}
+
+bool checkEndPacket(char* buffer){
+  for(int index = 12; index < BUFFSIZE; index++){
+    if(buffer[index] != 0) return false;
+  }
+  return true;
+}
+
+void copyBufferData(char* file_data, char* buffer, int& iterator){
+  for(int index = 12; index < BUFFSIZE; index++){
+    file_data[iterator++] = buffer[index];
+  }
+}
 
 int main(int argc, char const* argv[]){
   if(argc != 4){
     cout<<"Invalid number of parameters."<<endl;
     exit(EXIT_FAILURE);
   }
+
+  string server_host = string(argv[1]);
+  int server_port = atoi(argv[2]);
+  string file_name = string(argv[3]);
+
+  int sock_id;
+
+  struct sockaddr_in server_address;
+  socklen_t addrlen = sizeof(server_address);
+  if((sock_id = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+  	cout<<"Error in creating the socket."<<endl;
+  	exit(EXIT_FAILURE);
+  }
+
+  struct timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = TIMEOUT_MS;
+  if (setsockopt(sock_id, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
+      perror("Error");
+  }
+
+  memset((char*)&server_address, 0, sizeof(server_address));
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(server_port);
+
+  if(inet_pton(AF_INET, server_host.c_str(), &server_address.sin_addr) < 0){
+  	cout<<"Invalid address. This address is not supported."<<endl;
+	   exit(EXIT_FAILURE);
+  }
+
+  unsigned int sequenceNumber = 54;
+  unsigned int acknowledgementNumber = 20;
+  unsigned int receiveWindow = RECVWINDOW;
+  char* buffer = (char*)calloc(BUFFSIZE, sizeof(char));
+  generateRequest(file_name, buffer, sequenceNumber, acknowledgementNumber, receiveWindow);
+
+
+  if(sendto(sock_id, buffer, BUFFSIZE, 0, (struct sockaddr*) &server_address, addrlen) < 0){
+    perror("Error");
+    cout<<"Sending Failed"<<endl;
+    close(sock_id);
+    exit(EXIT_FAILURE);
+  }
+
+  char* file_data = (char*)calloc(MAXFILESIZE, sizeof(char));
+  bzero(buffer, BUFFSIZE);
+  bool flag = false;
+  int iterator = 0;
+  while(!flag && (recvfrom(sock_id, buffer, BUFFSIZE, 0, (struct sockaddr*)&server_address, &addrlen) > 0)){
+    flag = checkEndPacket(buffer);
+    copyBufferData(file_data, buffer, iterator);
+  }
+
+  cout<<file_data<<endl;
+
+  close(sock_id);
+  free(buffer);
 }
--- Server/server.cpp
+++ Server/server.cpp
@@ -3,10 +3,96 @@
 #include <netinet/in.h>
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
+#include <unistd.h>
 
-#define BUFFSIZE 1456
+#define DATASIZE 1456
+#define BUFFSIZE 1568
+#define RECVWINDOW 65535
+#define MAXFILESIZE 10000000
 
 using namespace std;
+
+string parseRequest(char* buffer){
+  unsigned char bytes[4];
+  int iterator = 0;
+  bytes[0] = buffer[iterator++];
+  bytes[1] = buffer[iterator++];
+  bytes[2] = buffer[iterator++];
+  bytes[3] = buffer[iterator++];
+  unsigned int sequenceNumber = (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | (bytes[3]);
+  cout<<sequenceNumber<<endl;
+  bytes[0] = buffer[iterator++];
+  bytes[1] = buffer[iterator++];
+  bytes[2] = buffer[iterator++];
+  bytes[3] = buffer[iterator++];
+
+  unsigned int acknowledgementNumber = (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | (bytes[3]);
+  cout<<acknowledgementNumber<<endl;
+  bytes[0] = buffer[iterator++];
+  bytes[1] = buffer[iterator++];
+  bytes[2] = buffer[iterator++];
+  bytes[3] = buffer[iterator++];
+
+  unsigned int receiveWindow = (bytes[0] << 8) | (bytes[1]);
+  cout<<receiveWindow<<endl;
+  string file_name = "";
+  while(buffer[iterator] != '\0'){
+    file_name+=string(1, buffer[iterator++]);
+  }
+  return file_name;
+}
+
+void readFile(char* file_data, string file_name, int& file_size){
+  ifstream file_id(file_name.c_str());
+
+  if(!file_id){
+    cout<<"Could not find "<<file_name<<"."<<endl;
+    file_size = -1;
+    return;
+  }
+  else{
+    string temp = "";
+    int iterator = 0;
+    while(getline(file_id, temp)){
+      temp+="\n";
+      memcpy(file_data+iterator, temp.c_str(), temp.size());
+      iterator+=temp.size();
+    }
+    file_size = iterator;
+    file_id.close();
+  }
+}
+
+void generateResponse(char* buffer, char* file_data, int file_size, int& byte_index, unsigned int sequenceNumber, unsigned int acknowledgementNumber, unsigned int receiveWindow){
+  unsigned char bytes[4];
+  bytes[0] = (sequenceNumber >> 24) & 0XFF;
+  bytes[1] = (sequenceNumber >> 16) & 0XFF;
+  bytes[2] = (sequenceNumber >> 8) & 0XFF;
+  bytes[3] = sequenceNumber & 0XFF;
+
+
+
+  memcpy(buffer, bytes, 4);
+
+
+  bytes[0] = (acknowledgementNumber >> 24) & 0XFF;
+  bytes[1] = (acknowledgementNumber >> 16) & 0XFF;
+  bytes[2] = (acknowledgementNumber >> 8) & 0XFF;
+  bytes[3] = acknowledgementNumber & 0XFF;
+
+  memcpy(buffer+4, bytes, 4);
+  bytes[0] = (receiveWindow >> 8) & 0XFF;
+  bytes[1] = receiveWindow & 0XFF;
+  bytes[2] = 0;
+  bytes[3] = 0;
+
+  memcpy(buffer+8, bytes, 4);
+
+  int cur_size = min(DATASIZE, file_size - byte_index);
+  memcpy(buffer+12, file_data+byte_index, cur_size);
+  byte_index+=cur_size;
+}
 
 int main(int argc, char const* argv[]){
     if(argc < 2){
@@ -45,12 +131,52 @@ int main(int argc, char const* argv[]){
 
     int recvlen;
     char* buffer = (char*)calloc(BUFFSIZE, sizeof(char));
+    char* file_data = (char*)calloc(MAXFILESIZE, sizeof(char));
     struct sockaddr_in remaddr;
     socklen_t raddrlen = sizeof(remaddr);
 
     while(1){
       cout<<"Receiving on port: "<<portNumber<<endl;
+      unsigned int sequenceNumber = 0;
+      unsigned int acknowledgementNumber = 0;
+      unsigned int receiveWindow = RECVWINDOW;
       recvlen = recvfrom(server_fd, buffer, BUFFSIZE, 0, (struct sockaddr*)&remaddr, &raddrlen);
+
+
+      string file_name = parseRequest(buffer);
+      bzero(buffer, BUFFSIZE);
+      int file_size = 0;
+      readFile(file_data, file_name, file_size);
+
+      int byte_index = 0;
+      while(byte_index < file_size){
+        generateResponse(buffer, file_data, file_size, byte_index, sequenceNumber, acknowledgementNumber, receiveWindow);
+
+        if(sendto(server_fd, buffer, BUFFSIZE, 0, (struct sockaddr*)&remaddr, raddrlen) < 0){
+          perror("Error:");
+          cout<<"Sending Failed"<<endl;
+          close(server_fd);
+          free(buffer);
+          exit(EXIT_FAILURE);
+        }
+        bzero(buffer, BUFFSIZE);
+        sequenceNumber+=byte_index;
+      }
+
+      memset(file_data, 0, MAXFILESIZE);
+      int it = 0;
+      generateResponse(buffer, file_data, MAXFILESIZE, it, sequenceNumber, acknowledgementNumber, receiveWindow);
+      if(sendto(server_fd, buffer, BUFFSIZE, 0, (struct sockaddr*)&remaddr, raddrlen) < 0){
+        perror("Error:");
+        cout<<"Sending Failed"<<endl;
+        close(server_fd);
+        free(buffer);
+        exit(EXIT_FAILURE);
+      }
+      bzero(buffer, BUFFSIZE);
+      sequenceNumber+=1;
     }
+    close(server_fd);
+    free(buffer);
 
 }
